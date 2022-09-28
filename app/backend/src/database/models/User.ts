ab3ed6257de9d9671d3ad89af3efd0b8212ceea9
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-09-28 13:05:08 -0300

    feat: Model User

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  private _password!: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default Users;
