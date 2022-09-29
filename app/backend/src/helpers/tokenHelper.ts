3b24ca97e4f86c2d0b2f9df0844511692304f101
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-09-29 10:43:24 -0300

    feat: helper para criação e validação de token

---
+++ app/backend/src/helpers/tokenHelper.ts
@@ -0,0 +1,23 @@
+import * as JWT from 'jsonwebtoken';
+import 'dotenv/config';
+import CustomError from '../errors/CustomError';
+
+const { JWT_SECRET } = process.env;
+const JWT_CONFIG: object = { algorithm: 'HS256', expiresIn: '1d' };
+
+const tokenHelper = {
+  createToken: (payload: object) => {
+    const token = JWT.sign(payload, JWT_SECRET as string, JWT_CONFIG);
+    return token;
+  },
+  verifyToken: (payload: string) => {
+    try {
+      const dados = JWT.verify(payload, JWT_SECRET as string);
+      return dados;
+    } catch (error) {
+      if (error) throw new CustomError(401, 'Expired or invalid token');
+    }
+  },
+};
+
+export default tokenHelper;
