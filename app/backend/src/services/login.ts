80dfcfb16ff47e35e90d1f9baab73e3bbead9bf0
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-09-29 11:59:48 -0300

    refactor: message de retorno do erro caso email e/ou password inválido

--- app/backend/src/services/login.ts
+++ app/backend/src/services/login.ts
@@ -8,10 +8,10 @@ export default class LoginService {
 
   public async login(email: string, password: string) {
     const user = await this._userModel.findOne({ where: { email } });
-    if (!user) throw new CustomError(404, 'User not found');
+    if (!user) throw new CustomError(401, 'Incorrect email or password');
 
     const comparePassword = bcrypt.compare(user.password, password);
-    if (!comparePassword) throw new CustomError(400, 'Unauthorized user');
+    if (!comparePassword) throw new CustomError(401, 'Incorrect email or password');
 
     const token = tokenHelper.createToken({ email });
 
