dbeff40d2612b664e6ced7616034bba23ce2cdbf
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-09-30 14:54:31 -0300

    perf: lógica de verificação para a chamada de método correspondente na requisição

--- app/backend/src/controllers/match.ts
+++ app/backend/src/controllers/match.ts
@@ -1,19 +1,31 @@
 import { Request, Response } from 'express';
 import MatchService from '../services/match';
 
+interface Query {
+  inProgress: string;
+}
+
 export default class MatchController {
   private _matchService = new MatchService();
 
   public async getAll(req: Request, res: Response) {
+    const { inProgress } = req.query as unknown as Query;
+
+    if (inProgress) {
+      const result = await this._matchService.getByProgress(inProgress);
+
+      return res.status(200).json(result);
+    }
+
     const result = await this._matchService.getAll();
 
     return res.status(200).json(result);
   }
 
-  // public async getById(req: Request, res: Response) {
-  //   const { id } = req.params;
+  // public async getByProgress(req: Request, res: Response) {
+  //   const { inProgress } = req.query as unknown as Query;
 
-  //   const result = await this._teamService.getById(Number(id));
+  //   const result = await this._matchService.getByProgress(inProgress);
 
   //   return res.status(200).json(result);
   // }
