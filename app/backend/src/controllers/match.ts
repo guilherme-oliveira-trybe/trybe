0da115a56573797bab072c5261fe2f6393342e6a
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-09-30 16:02:14 -0300

    feat: método create ao controller de match

--- app/backend/src/controllers/match.ts
+++ app/backend/src/controllers/match.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Create } from '../interfaces/match';
 import MatchService from '../services/match';
 
 interface Query {
@@ -20,6 +21,14 @@ export default class MatchController {
     const result = await this._matchService.getAll();
 
     return res.status(200).json(result);
+  }
+
+  public async create(req: Request, res: Response) {
+    const info = req.body as Create;
+
+    const result = await this._matchService.create(info);
+
+    return res.status(201).json(result);
   }
 
   // public async getByProgress(req: Request, res: Response) {
