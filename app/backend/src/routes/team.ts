ac11bb320fcad229b8abd00b4603bfc6be10bfc8
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-09-30 09:31:32 -0300

    feat: rota /teams/:id

--- app/backend/src/routes/team.ts
+++ app/backend/src/routes/team.ts
@@ -5,5 +5,6 @@ const teamController = new TeamController();
 const teamRoute = Router();
 
 teamRoute.get('/', (req, res) => teamController.getAll(req, res));
+teamRoute.get('/:id', (req, res) => teamController.getById(req, res));
 
 export default teamRoute;
