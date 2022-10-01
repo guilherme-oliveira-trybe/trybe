bd9c830d1be87e5d0ce24437bb679ab389fd68ec
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-01 10:29:09 -0300

    feat: método de atualização do progresso da partida no controller da rota /matches/:id/finish

--- app/backend/src/controllers/match.ts
+++ app/backend/src/controllers/match.ts
@@ -30,4 +30,12 @@ export default class MatchController {
 
     return res.status(201).json(result);
   }
+
+  public async updateMatchProgress(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const result = await this._matchService.updateMatchProgress(Number(id));
+
+    return res.status(200).json(result);
+  }
 }
