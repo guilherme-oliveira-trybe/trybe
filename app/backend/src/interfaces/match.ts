a2f0f673ce0a8bf62a05ac20dfbd4a5a16e349da
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-04 15:55:09 -0300

    feat: interface para utlização na rota /leaderboard/home

--- app/backend/src/interfaces/match.ts
+++ app/backend/src/interfaces/match.ts
@@ -8,3 +8,27 @@ export interface Create extends UpdateInfo {
   awayTeam: number;
   inProgress: string;
 }
+
+export interface InfoMatches extends Create {
+  id: number;
+}
+
+export interface TeamInfoMatches {
+  id: number;
+  teamName: string;
+  homeTeam: InfoMatches[];
+  awayTeam: InfoMatches[];
+}
+
+export interface MatchesResult {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: number,
+}
