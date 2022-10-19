3dc29550f82415c4ed574b8ddebe4042479bd8f2
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-19 12:15:26 -0300

    refactor: aplica principio S do Solid, refatorando funções genéricas

--- app/backend/src/services/leaderBoard.ts
+++ app/backend/src/services/leaderBoard.ts
@@ -42,92 +42,25 @@ export default class LeaderBoardService {
     return result as unknown as TeamInfoMatches[];
   }
 
-  private checkInfoHome = (info: InfoMatches[]) => {
-    let rng = {
-      points: 0, victories: 0, draws: 0, losses: 0, goalsFavor: 0, goalsOwn: 0 };
-
-    info.forEach((match) => {
-      if (match.homeTeamGoals === match.awayTeamGoals) {
-        rng = { ...rng, draws: rng.draws + 1, points: rng.points + 1 };
-      } else if (match.homeTeamGoals > match.awayTeamGoals) {
-        rng = { ...rng, victories: rng.victories += 1, points: rng.points += 3 };
-      } else { rng.losses += 1; }
-
-      rng.goalsFavor += match.homeTeamGoals;
-      rng.goalsOwn += match.awayTeamGoals;
-    });
-
-    const totalGames = info.length;
-    const goalsBalance = rng.goalsFavor - rng.goalsOwn;
-    const efficiency = Number(((rng.points / (totalGames * 3)) * 100).toFixed(2));
-
-    return { ...rng, totalGames, goalsBalance, efficiency };
-  };
-
-  private checkInfoAway = (info: InfoMatches[]) => {
-    let rng = {
-      points: 0, victories: 0, draws: 0, losses: 0, goalsFavor: 0, goalsOwn: 0 };
-
-    info.forEach((match) => {
-      if (match.awayTeamGoals === match.homeTeamGoals) {
-        rng = { ...rng, draws: rng.draws + 1, points: rng.points + 1 };
-      } else if (match.awayTeamGoals > match.homeTeamGoals) {
-        rng = { ...rng, victories: rng.victories += 1, points: rng.points += 3 };
-      } else { rng.losses += 1; }
-
-      rng.goalsFavor += match.awayTeamGoals;
-      rng.goalsOwn += match.homeTeamGoals;
-    });
-
-    const totalGames = info.length;
-    const goalsBalance = rng.goalsFavor - rng.goalsOwn;
-    const efficiency = Number(((rng.points / (totalGames * 3)) * 100).toFixed(2));
-
-    return { ...rng, totalGames, goalsBalance, efficiency };
-  };
-
-  private handleInfo = (array: TeamInfoMatches[], status: FieldType) => {
-    const infoHome = array.map((team) => {
-      const check = status === 'home'
-        ? this.checkInfoHome(team.homeTeam) : this.checkInfoAway(team.awayTeam);
-
-      const result = {
-        name: team.teamName,
-        totalPoints: check.points,
-        totalGames: check.totalGames,
-        totalVictories: check.victories,
-        totalDraws: check.draws,
-        totalLosses: check.losses,
-        goalsFavor: check.goalsFavor,
-        goalsOwn: check.goalsOwn,
-        goalsBalance: check.goalsBalance,
-        efficiency: check.efficiency,
-      };
-
-      return result;
-    });
-    return infoHome;
-  };
-
   private handleGeneralRanking = (array: TeamInfoMatches[]) => {
     const infoHome = array.map((team) => {
-      const matchesHome = this.checkInfoHome(team.homeTeam);
-      const matchesAway = this.checkInfoAway(team.awayTeam);
+      const matchesHome = this.handleResults(team.homeTeam, 'home'); // tirar status
+      const matchesAway = this.handleResults(team.awayTeam, 'away'); // tirar status
 
       const result = {
         name: team.teamName,
-        totalPoints: matchesHome.points + matchesAway.points,
+        totalPoints: matchesHome.totalPoints + matchesAway.totalPoints,
         totalGames: matchesHome.totalGames + matchesAway.totalGames,
-        totalVictories: matchesHome.victories + matchesAway.victories,
-        totalDraws: matchesHome.draws + matchesAway.draws,
-        totalLosses: matchesHome.losses + matchesAway.losses,
+        totalVictories: matchesHome.totalVictories + matchesAway.totalVictories,
+        totalDraws: matchesHome.totalDraws + matchesAway.totalDraws,
+        totalLosses: matchesHome.totalLosses + matchesAway.totalLosses,
         goalsFavor: matchesHome.goalsFavor + matchesAway.goalsFavor,
         goalsOwn: matchesHome.goalsOwn + matchesAway.goalsOwn,
-        goalsBalance: matchesHome.goalsBalance + matchesAway.goalsBalance,
       };
 
       return { ...result,
-        efficiency: Number(((result.totalPoints / (result.totalGames * 3)) * 100).toFixed(2)) };
+        goalsBalance: this.handleGoalsBalance(result.goalsFavor, result.goalsOwn),
+        efficiency: this.handleEfficiency(result.totalPoints, result.totalGames) };
     });
     return infoHome;
   };
@@ -139,5 +72,119 @@ export default class LeaderBoardService {
     const sortByTotalPoints = sortByGoalsBalance.sort((a, b) => b.totalPoints - a.totalPoints);
 
     return sortByTotalPoints;
+  };
+
+  private handleInfo = (array: TeamInfoMatches[], status: FieldType) => {
+    const infoHome = array.map((team) => {
+      const check = status === 'home'
+        ? this.handleResults(team.homeTeam, status) : this.handleResults(team.awayTeam, status);
+
+      const result = {
+        name: team.teamName,
+        totalPoints: check.totalPoints,
+        totalGames: check.totalGames,
+        totalVictories: check.totalVictories,
+        totalDraws: check.totalDraws,
+        totalLosses: check.totalLosses,
+        goalsFavor: check.goalsFavor,
+        goalsOwn: check.goalsOwn,
+        goalsBalance: this.handleGoalsBalance(check.goalsFavor, check.goalsOwn),
+        efficiency: this.handleEfficiency(check.totalPoints, check.totalGames),
+      };
+
+      return result;
+    });
+    return infoHome;
+  };
+
+  private handleResults = (info: InfoMatches[], status: FieldType) => {
+    const resultMatches = info.reduce((acc, match) => {
+      const result = this.handleSumInfos(match, status);
+      acc.totalPoints += result.totalPoints;
+      acc.totalVictories += result.totalVictories;
+      acc.totalDraws += result.totalDraws;
+      acc.totalLosses += result.totalLosses;
+      acc.goalsFavor += result.goalsFavor;
+      acc.goalsOwn += result.goalsOwn;
+
+      return acc;
+    }, {
+      totalPoints: 0,
+      totalVictories: 0,
+      totalDraws: 0,
+      totalLosses: 0,
+      goalsFavor: 0,
+      goalsOwn: 0,
+    });
+
+    return { ...resultMatches, totalGames: info.length };
+  };
+
+  private handleSumInfos = (match: InfoMatches, status: FieldType) => {
+    const tiedMatch = this.handleTiedMatch(match.homeTeamGoals, match.awayTeamGoals); // passar status partida aqui
+    const victoreMatch = status === 'home'
+      ? this.handleVictoryMatch(match.homeTeamGoals, match.awayTeamGoals)
+      : this.handleVictoryMatch(match.awayTeamGoals, match.homeTeamGoals);
+
+    const lossMatch = status === 'home'
+      ? this.handleLosseMatch(match.homeTeamGoals, match.awayTeamGoals)
+      : this.handleLosseMatch(match.awayTeamGoals, match.homeTeamGoals);
+
+    const result = {
+      totalPoints: tiedMatch.points + victoreMatch.points,
+      totalVictories: victoreMatch.victories,
+      totalDraws: tiedMatch.draws,
+      totalLosses: lossMatch,
+      goalsFavor: status === 'home' ? match.homeTeamGoals : match.awayTeamGoals,
+      goalsOwn: status === 'home' ? match.awayTeamGoals : match.homeTeamGoals,
+    }; // fazer as somas aqui
+
+    return result;
+  };
+
+  private handleTiedMatch = (goalsFavor: number, goalsOwn: number) => {
+    let draws = 0;
+    let points = 0;
+
+    if (goalsFavor === goalsOwn) {
+      draws += 1;
+      points += 1;
+    }
+
+    return { draws, points };
+  };
+
+  private handleVictoryMatch = (goalsFavor: number, goalsOwn: number) => {
+    let victories = 0;
+    let points = 0;
+
+    if (goalsFavor > goalsOwn) {
+      victories += 1;
+      points += 3;
+    }
+
+    return { victories, points };
+  };
+
+  private handleLosseMatch = (goalsFavor: number, goalsOwn: number) => {
+    let losses = 0;
+
+    if (goalsFavor < goalsOwn) {
+      losses += 1;
+    }
+
+    return losses;
+  };
+
+  private handleGoalsBalance = (totalGoalsFavor: number, totalGoalsOwn: number) => {
+    const goalsBalanceResult = totalGoalsFavor - totalGoalsOwn;
+
+    return goalsBalanceResult;
+  };
+
+  private handleEfficiency = (totalPoints: number, totalGames: number) => {
+    const efficiencyResult = Number(((totalPoints / (totalGames * 3)) * 100).toFixed(2));
+
+    return efficiencyResult;
   };
 }
